f9264682ba0c700fbdb9a155462babc271b71a5d
Ubuntu <ubuntu@ip-172-31-43-69.us-west-2.compute.internal>
2016-09-20 01:41:17 +0000

    going to use multipart upload

--- webapp/server.js
+++ webapp/server.js
@@ -2,16 +2,65 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var AWS = require('aws-sdk');
+var request = require('request');
 AWS.config.loadFromPath('./config.json');
 AWS.config.update({region: 'us-west-2'});
 
 var s3 = new AWS.S3();
 var params = {Bucket: 'cs499rbucket', Key: 'tweet'};
 var params2 = {Bucket: 'cs499rbucket'};
-s3.headObject(params, function (err, data) {
-  if (err) console.log(err, err.stack);
-  else console.log(data.ContentLength);
-});
+
+var pieceSize = 1024 * 1024 * 5;
+
+//////////////// MULTI UPLOAD STUFF
+
+function completeMultipartUpload(s3, doneParams) {
+  s3.completeMultipartUpload(doneParams, function(err, data) {
+    if (err) {
+      console.log("An error occurred while completing the multipart upload");
+      console.log(err);
+    } else {
+      var delta = (new Date() - startTime) / 1000;
+      console.log('Completed upload in', delta, 'seconds');
+      console.log('Final upload data:', data);
+    }
+  });
+}
+
+function uploadPart(s3, multipart, partParams, tryNum) {
+  var tryNum = tryNum || 1;
+  s3.uploadPart(partParams, function(multiErr, mData) {
+    if (multiErr){
+      console.log('multiErr, upload part error:', multiErr);
+      if (tryNum < maxUploadTries) {
+        console.log('Retrying upload of part: #', partParams.PartNumber)
+        uploadPart(s3, multipart, partParams, tryNum + 1);
+      } else {
+        console.log('Failed uploading part: #', partParams.PartNumber)
+      }
+      return;
+    }
+    multipartMap.Parts[this.request.params.PartNumber - 1] = {
+      ETag: mData.ETag,
+      PartNumber: Number(this.request.params.PartNumber)
+    };
+    console.log("Completed part", this.request.params.PartNumber);
+    console.log('mData', mData);
+    if (--numPartsLeft > 0) return; // complete only when all parts uploaded
+
+    var doneParams = {
+      Bucket: bucket,
+      Key: fileKey,
+      MultipartUpload: multipartMap,
+      UploadId: multipart.UploadId
+    };
+
+    console.log("Completing upload...");
+    completeMultipartUpload(s3, doneParams);
+  });
+}
+
+///////////////////////////////
 
 
 app.get('/', function(req, res){
@@ -27,21 +76,62 @@ io.on('connection', function(socket){
     }
   });
   socket.on('filter', function(fn) {
-    // check s3 meta data  
-    // break down into pieces
-    // send multiple post requests
-/*
-var request = require('request');
+    console.log(fn.key);
+    myparams = {Bucket: 'cs499rbucket', Key: fn.key}
+    mylength = 0;
+    s3.headObject(myparams, function(err, data) {
+      if (err) {
+        console.log("ERROR GETTING METADATA");
+        console.log(err. err.stack);
+      }
+      else {
+        mylength = parseInt(data.ContentLength)
+	pieces = Math.ceil(mylength/pieceSize); 
 
-request.post(
-    'http://www.yoursite.com/formpage',
-    { json: { key: 'value' } },
-    function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body)
+        var partNum = 0;
+
+        multiPartParams = {
+          Bucket: 'cs499rbucket',
+          key : fn.Key,
+          //ContentType: 'application/octet-stream'
         }
-    }
-);
+        myUploadId = "";
+        s3.createMultipartUpload(multiPartParams, function(mpErr, multipart){
+          if (mpErr) { console.log('Error!', mpErr); return; }
+          console.log("Got upload ID", multipart.UploadId);
+          myUploadId = UploadId; 
+        });
+
+        console.log("PIECES: " + pieces);
+        piecesCompleted = 0;
+        for (i = 0 ; i < pieces ; i++) {
+          range1 = pieceSize * i;
+          range2 = pieceSize * (i + 1) - 1;
+          range1 = range1.toString()
+          range2 = range2.toString()
+          request.post(
+              'https://tm8k880tf3.execute-api.us-west-2.amazonaws.com/prod/s3grab',
+              { json: { function: fn.function, range: "bytes=" + range1 + "-" + range2, part: i, key: fn.key, uploadId: myUploadId} },
+              function (error, response, body) {
+                  if (!error && response.statusCode == 200) {
+                      console.log(body);
+                      piecesCompleted += 1;
+                      console.log("PIECESCOmpleted: " + piecesCompleted);
+                      if (piecesCompleted == pieces) {
+                        console.log("Complete");
+                        // merge that shit
+                      }
+                  } else {
+                      console.log("ERROR");
+                      console.log("Part" + i+1 );
+                  }
+              }
+          );
+        }
+      }
+    });
+/*
+
 */
   });
   socket.on('key', function(key) {
