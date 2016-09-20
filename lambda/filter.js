541ba30cc418b4e0b5b2a6ebf1d05ea3fcb36e9d
Ubuntu <ubuntu@ip-172-31-43-69.us-west-2.compute.internal>
2016-09-20 07:07:13 +0000

    update filter

--- lambda/filter.js
+++ lambda/filter.js
@@ -4,12 +4,51 @@ console.log('Loading function');
 let aws = require('aws-sdk');
 let s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
+function CSVtoArray(text) {
+    var re_valid = /^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*)*$/;
+    var re_value = /(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g;
+    // Return NULL if input string is not well formed CSV string.
+    if (!re_valid.test(text)) return null;
+    var a = [];                     // Initialize array to receive values.
+    text.replace(re_value, // "Walk" the string using replace with callback.
+        function(m0, m1, m2, m3) {
+            // Remove backslash from \' in single quoted values.
+            if      (m1 !== undefined) a.push(m1.replace(/\\'/g, "'"));
+            // Remove backslash from \" in double quoted values.
+            else if (m2 !== undefined) a.push(m2.replace(/\\"/g, '"'));
+            else if (m3 !== undefined) a.push(m3);
+            return ''; // Return empty string.
+        });
+    // Handle special case of empty last value.
+    if (/,\s*$/.test(text)) a.push('');
+    return a;
+}
+
+
 exports.handler = (event, context, callback) => {
     //console.log('Received event:', JSON.stringify(event, null, 2));
-
+    function uploadPart(s3, upload_param, tryNum) {
+    
+        s3.uploadPart(upload_param, function(err, data) {
+            if (err) {
+                if (tryNum < 3) {
+                    console.log('Retrying upload of part: #', upload_param.PartNumber)
+                    // try again
+                    uploadPart(s3, upload_param, tryNum + 1);
+                    callback(err, err.stack);
+                } else {
+                    callback(err, err.stack);
+                }
+            } // an error occurred
+            else {
+                callback(null, {ETag: data.ETag, Part: event.part});           // successful response
+            }
+            context.done();
+        });
+    }
     // Get the object from the event and show its content type
     const bucket = 'cs499rbucket';
-    const key = decodeURIComponent('tweet'.replace(/\+/g, ' '));
+    const key = decodeURIComponent(event.key.replace(/\+/g, ' '));
     const params = {
         Bucket: bucket,
         Key: key,
@@ -37,19 +76,25 @@ exports.handler = (event, context, callback) => {
         } else {
             console.log('CONTENT TYPE:', data.ContentType);
             var body = data.Body;
+            console.log('body.toString()');
             var mydata = body.toString();
+            console.log('split');
             mydata = mydata.split(/\n/);
+            console.log('filter');
             mydata = mydata.filter(filterword(event.function));
-            mydata = mydata.toString();
+            console.log('toString');
+            mydata = mydata.join('\n');
             //var file = require('fs').createWriteStream('/path/to/file.jpg');
             //s3.getObject(params).createReadStream().pipe(file);
-
-            var upload_param = {Bucket: bucket, Key: key+"part"+event.part, Body: mydata};
-            s3.upload(upload_param, function(err, data) {
-                if (err) callback(err, err.stack); // an error occurred
-                else callback(null, data);           // successful response
-                context.done();
-            });
+            var tryNum = 0;
+            var upload_param = {
+              Body: mydata,
+              Bucket: bucket,
+              Key: event.output,
+              PartNumber: event.part,
+              UploadId: event.uploadId
+            };
+            uploadPart(s3, upload_param, tryNum)
         }
     });
     
